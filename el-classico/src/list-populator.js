d3f26d7c114c74dd18314a80bec10296d1704840
Clark Chan <clarkchan@Clarks-MBP.lan>
2022-07-06 11:36:36 -0400

    initial commit

---
+++ el-classico/src/list-populator.js
@@ -0,0 +1,37 @@
+const cesspool = [
+  "dead serious",
+  "stopwatch ⏱️ on fleek",
+  "Node is a runtime environment don't @ me",
+  "The browser is asynchronous no 🧢",
+  "useState is Big Yikes 🤮",
+  "useCallback🔥",
+  "Adaptive has the coldest drip 🥶🥶",
+  "chicken nuggets",
+  "conditional rendering is BUSSIN 💯",
+];
+
+const listContainer = document.querySelector(".list-container");
+
+function createListItem() {
+  const listItem = document.createElement("div");
+  const B = document.createElement("p");
+  const msg = document.createElement("p");
+
+  listItem.classList.add("list-item-container");
+  B.classList.add("B");
+  B.innerHTML = "B";
+  msg.classList.add("list-message");
+  msg.innerHTML = cesspool[Math.floor(Math.random() * cesspool.length)];
+  listItem.appendChild(B);
+  listItem.appendChild(msg);
+
+  return listItem;
+}
+
+function createList() {
+  for (let a = 0; a < 20; a++) {
+    listContainer.appendChild(createListItem());
+  }
+}
+
+createList();
